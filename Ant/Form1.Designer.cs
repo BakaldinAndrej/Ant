5f52942bbe7c612f8785201ae3424ae3bab8b4ec
Bakaldin Andrej <bakaldin.andrej@yandex.ru>
2018-11-20 22:22:59 +0300

    Initial commit

---
+++ Ant/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Ant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this._tbAnswer = new System.Windows.Forms.TextBox();
+            this._tbError = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this._btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv.Location = new System.Drawing.Point(12, 12);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 42;
+            this.dgv.Size = new System.Drawing.Size(373, 244);
+            this.dgv.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ответ";
+            // 
+            // _tbAnswer
+            // 
+            this._tbAnswer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this._tbAnswer.Location = new System.Drawing.Point(137, 266);
+            this._tbAnswer.Name = "_tbAnswer";
+            this._tbAnswer.Size = new System.Drawing.Size(100, 20);
+            this._tbAnswer.TabIndex = 2;
+            // 
+            // _tbError
+            // 
+            this._tbError.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this._tbError.Location = new System.Drawing.Point(314, 266);
+            this._tbError.Name = "_tbError";
+            this._tbError.Size = new System.Drawing.Size(70, 20);
+            this._tbError.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Конфликты";
+            // 
+            // _btnStart
+            // 
+            this._btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this._btnStart.Location = new System.Drawing.Point(13, 263);
+            this._btnStart.Name = "_btnStart";
+            this._btnStart.Size = new System.Drawing.Size(75, 23);
+            this._btnStart.TabIndex = 5;
+            this._btnStart.Text = "Старт";
+            this._btnStart.UseVisualStyleBackColor = true;
+            this._btnStart.Click += new System.EventHandler(this._btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 297);
+            this.Controls.Add(this._btnStart);
+            this.Controls.Add(this._tbError);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this._tbAnswer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv);
+            this.Name = "Form1";
+            this.Text = "Король муравьев";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox _tbAnswer;
+        private System.Windows.Forms.TextBox _tbError;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button _btnStart;
+    }
+}
+
---
+++ Ant/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ant
+{
+    public partial class Form1 : Form
+    {
+        const int N = 8;
+        const int Q = 100;
+        const double a = 0.5;
+        const double b = 5.0;
+        const double pheromonInit = 1.0 / N;
+        const int antCount = 8;
+        Random rand;
+        int[] mas = new int[N];
+        double[,] niff = new double[N, N];
+        int[,] tabu = new int[N, N];
+
+        public Form1()
+        {
+            InitializeComponent();
+            dgv.RowCount = dgv.ColumnCount = N;
+            for (int i = 0; i < N; i++)
+            {
+                dgv.Columns[i].HeaderText = (i + 1).ToString();
+                for (int j = 0; j < N; j++)
+                    dgv[i, j].Value = pheromonInit;
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    niff[i, j] = pheromonInit;
+                }
+                tabu[i, i] = 1;
+                mas[i] = i;
+            }
+
+            rand = new Random();
+        }
+
+        private int CalcErrors()
+        {
+            int e = 0;
+            Point[] d = { new Point(-1, -1), new Point(1, 1), new Point(-1, 1), new Point(1, -1) };
+            int tx = 0;
+            int ty = 0;
+
+            for (int x = 0; x < N; x++)
+            {
+                for (int j = 0; j < N; j++)
+                    if ((mas[x] == mas[j]) && (x != j))
+                        e++;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    tx = x + d[i].X;
+                    ty = mas[x] + d[i].Y;
+
+                    while ((tx >= 0) && (tx < N) && (ty >= 0) && (ty < N))
+                    {
+                        if (mas[tx] == ty) e++;
+
+                        tx += d[i].X;
+                        ty += d[i].Y;
+                    }
+                }
+            }
+
+            return e;
+        }
+
+        double antProduct(int i, int j)
+        {
+            return ((Math.Pow(niff[i, j], a) * Math.Pow((1.0 / CalcErrors()), b)));
+        }
+
+        private int SelectNaxtCell(int ant)
+        {
+            double sum = 0;
+            for (int j = 0; j < N; j++)
+            {
+                if (tabu[ant, j] == 0) sum += niff[ant, j];
+            }
+
+            double p = 0;
+            double[] pp = new double[N];
+            int answ=0;
+            for (int j = 0; j < N; j++)
+            {
+                if (tabu[ant, j] == 0)
+                {
+                    pp[j] = antProduct(ant, j) / sum;
+                    p += pp[j];
+                }
+            }
+            double temp = rand.NextDouble() * p;
+
+            p = 0;
+            for (int j=0;j<N;j++)
+            {
+                if ((temp > p)&&(tabu[ant,j] == 0))
+                    answ = j;
+                p += pp[j];
+            }
+            return answ;
+        }
+
+        private void AntCalc()
+        {
+            for (int step = 0; step < 100; step++)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    mas[i] = SelectNaxtCell(i);
+                    tabu[i, mas[i]] = 1;
+                    int temp = 0;
+                    for (int j = 0; j < N; j++)
+                        if (tabu[i, j] == 1)
+                            temp++;
+                    if (temp == N)
+                    {
+                        for (int j = 0; j < N; j++)
+                            tabu[i, j] = 0;
+                        tabu[i, mas[i]] = 1;
+                    }
+
+                    for (int j = 0; j < N; j++)
+                    {
+                        niff[i, j] = niff[i, j] * 0.5 + Q / CalcErrors();
+                    }
+                }
+            }
+        }
+
+        private void _btnStart_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            AntCalc();
+            Cursor.Current = Cursors.Default;
+
+            _tbAnswer.Text = "";
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    dgv[i, j].Value = niff[i, j];
+                }
+                _tbAnswer.Text += mas[i];
+            }
+
+            _tbError.Text = CalcErrors().ToString();
+        }
+    }
+}
